cf9c4e8f15c72ca0662d57985aa32a3ba4bb1548
Bappa Sarkar <find.bappa@gmail.com>
2017-02-19 08:51:32 -0500

    update

--- shortest_palindrome.php
+++ shortest_palindrome.php
@@ -6,18 +6,25 @@
  * Time: 8:12 AM
  */
 
-$str = "abbas";
+$str = "sabba";
 echo shortest_palindrome($str). "\n";
 
 function shortest_palindrome($str) {
     $total_char = strlen($str);
-    $extra_str = '';
+    $extra_str = '';$extra_str2='';
     if (is_palindrone($str)) {
         return $str;
     }
     for ($i=0; $i<$total_char; $i++) {
+        // Put the extra sting in front of the original string.
         $extra_str .= $str[$i];
         $test_str = $str.strrev($extra_str);
+        if (is_palindrone($test_str)) {
+            return $test_str;
+        }
+        // Put the extra sting at the end of the original string.
+        $extra_str2 .= $str[$total_char-$i-1];
+        $test_str = strrev($extra_str2).$str;
         if (is_palindrone($test_str)) {
             return $test_str;
         }
