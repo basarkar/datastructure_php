8dae775e5403c0f30fdcc1f7158bfe513287f53e
Bappa Sarkar <find.bappa@gmail.com>
2017-02-22 08:44:48 -0500

    merge sort

--- merge_sort.php
+++ merge_sort.php
@@ -7,18 +7,45 @@
  */
 
 $array = [3,6,2,4,9,1,5,8,0,7];
-mergeSort($array, 0, count($array)-1);
+echo "Original array: " . implode(', ', $array) . "\n";
+$array = mergeSort($array);
+echo "Merge sorted array: " . implode(', ', $array) . "\n";
 
-function mergeSort($array, $start, $end) {
-    echo implode(', ', array_slice($array, $start, $end-$start+1)) . "\n";
-    if ($start>=$end) {
+function mergeSort($array) {
+    // If the array has one or empty values then return it right away.
+    if (count($array) <= 1) {
         return $array;
     }
+    $start = 0; $end = count($array)-1;
     $mid = floor(($start + $end)/2);
-    $a = mergeSort($array, $start, $mid);
-    $b = mergeSort($array, $mid+1, $end);
-
+    // Run recursion on left set.
+    $a = mergeSort(array_slice($array, $start, $mid-$start+1));
+    // Run recursion on right set.
+    $b = mergeSort(array_slice($array, $mid+1, $end-$mid));
+    return merge($a, $b);
 }
-function merge($a, $b) {
 
+function merge($a, $b) {
+    $c = [];
+    $i=0; $a_n = count($a);
+    $j=0; $b_n = count($b);
+    while ($i<$a_n && $j<$b_n) {
+        if ($a[$i] > $b[$j]) {
+            $c[] = $b[$j];
+            $j++;
+        }
+        else {
+            $c[] = $a[$i];
+            $i++;
+        }
+    }
+    while ($i<$a_n) {
+        $c[] = $a[$i];
+        $i++;
+    }
+    while ($j<$b_n) {
+        $c[] = $b[$j];
+        $j++;
+    }
+    return $c;
 }
--- quick_sort.php
+++ quick_sort.php
@@ -7,19 +7,26 @@
  * Ref: https://www.youtube.com/watch?v=SLauY6PpjW4&t=9s
  */
 $array = [3,6,2,4,9,1,5,8,0,7];
+echo "Original array: " . implode(', ', $array) . "\n";
 quickSort($array, 0, count($array)-1);
 echo "Quick sorted array: " . implode(', ', $array) . "\n";
 
 function quickSort(&$array, $start, $end) {
+    // Base condition.
     if ($start >= $end) {
         return;
     }
+    // Get the pivot index (by shifting all less values left from pivot value
+    // and greater values in the right of the pivot value) to partition.
     $pivot_index = partition($array, $start, $end);
+    // Recursive Quick sort: Left set.
     quickSort($array, $start, $pivot_index-1);
+    // Recursive Quick sort: Right set.
     quickSort($array, $pivot_index, $end);
 }
 
 function partition(&$array, $start, $end) {
+    // Let say we set the pivot value from the middle of the array.
     $pivot = $array[floor(($start+$end)/2)];
     while ($start < $end) {
         while ($array[$start] < $pivot) {
