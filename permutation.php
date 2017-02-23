ab76c86b257423a05eeb7b14b00b6b2072e77cb7
Bappa Sarkar <bsarkar@dnainfo.com>
2017-02-23 15:02:57 -0500

    update

--- permutation.php
+++ permutation.php
@@ -1,22 +1,26 @@
 <?php
-$str = "abc";
+$str = "ABCD";
 $str = str_split($str);
 $result = permutation($str);
 
 print_R($result);
 
 function permutation($array) {
+  // Return the string if it is one character long.
   if (count($array) == 1) {
     return $array;
   }
-  $out = [];
+  $output = [];
+  // Foreach character create the permutation.
   foreach ($array as $k => $a) {
+    // fix the each value and get permutation of rest of the characters.
     $temp = $array;
     unset($temp[$k]);
     $permutation = permutation($temp);
+    // Concat each permutation with the fixed character (placed in first place).
     foreach ($permutation as $p) {
-      $out[] = $a . $p;
+      $output[] = $a . $p;
     }
   }
-  return $out;
+  return $output;
 }
