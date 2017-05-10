833638285fc057a9265b852114d49580a3a005ff
Bappa Sarkar <bsarkar@dnainfo.com>
2017-05-10 17:07:13 -0400

    update

---
+++ wildcard.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bappasarkar
+ * Date: 5/10/17
+ * Time: 3:50 PM
+ */
+
+/**
+ * The wild cards are * which will match any character 0 or more occurrence
+ * and ? which will match one character
+ * Input: pattern, string
+ * Output: Boolean (True/False)
+ */
+
+var_dump(wildcard_match('c*w', 'cauww'));
+
+function wildcard_match($pattern, $string) {
+  if ($pattern == '' || $string == '') {
+    return FALSE;
+  }
+  $string = str_split($string);
+  $pattern = str_split($pattern);
+
+  foreach ($string as $str) {
+    if (empty($pattern)) {
+      return FALSE;
+    }
+    switch (reset($pattern)) {
+      case '?':
+        array_shift($pattern);
+        break;
+      case '*':
+        // If we only have * then match all.
+        if (empty($pattern[1])) {
+          return TRUE;
+        }
+        else {
+          $next = $pattern[1];
+          if ($next == $str || $next == '?') {
+            // Remove *
+            array_shift($pattern);
+            // Remove next character.
+            array_shift($pattern);
+          }
+        }
+        break;
+      default:
+        if ($str !== $pattern[0]) {
+          return FALSE;
+        }
+        array_shift($pattern);
+        break;
+    }
+  }
+
+  // @Todo If pattern left other than * then fail.
+  if (!empty($pattern)) {
+    return FALSE;
+  }
+  else {
+    return TRUE;
+  }
+}
